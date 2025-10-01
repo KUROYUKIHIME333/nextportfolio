866553dfe086547748f2e117e8ddf18674005777
KUROYUKIHIME333 <dhermansum@gmail.com>
2025-10-01 23:48:22 +0100

    The entry point or layout, my app

---
+++ src/app/layout.jsx
@@ -0,0 +1,39 @@
+import { Poppins } from 'next/font/google';
+import { AppProvider } from '../contexts/AppProvider';
+import { Header } from '../components/organisms/Header';
+import { Footer } from '../components/organisms/Footer';
+import { ParticlesBG } from '../components/organisms/ParticlesBg';
+import { LoadingScreen } from '../components/organisms/LoadingScreen';
+import './globals.css';
+import Script from 'next/script';
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '600', '700'] });
+
+export const metadata = {
+	title: 'Daniel Ramazani - Portfolio',
+	description: 'Portfolio professionnel de Daniel RAMAZANI, Ingénieur technicien en Maintenance Industrielle et Développeur web.',
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="fr">
+			<head>
+				{/* Font Awesome for Icons */}
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+				<Script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js" strategy="beforeInteractive" />
+			</head>
+			<body className={poppins.className}>
+				<AppProvider>
+					<LoadingScreen />
+					<ParticlesBG />
+					<div className="relative z-20">
+						{/* What will be up to particles */}
+						<Header />
+						<main>{children}</main>
+						<Footer />
+					</div>
+				</AppProvider>
+			</body>
+		</html>
+	);
+}
