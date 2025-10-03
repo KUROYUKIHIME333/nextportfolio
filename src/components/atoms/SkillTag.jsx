86b009a626f1a7ca20f74940e2026ce8de51fdf6
KUROYUKIHIME333 <dhermansum@gmail.com>
2025-10-03 09:26:26 +0100

    Remove those bullshits

--- src/components/atoms/SkillTag.jsx
+++ src/components/atoms/SkillTag.jsx
@@ -1,5 +1,5 @@
 export const SkillTag = ({ children }) => {
 	return (
-		<span className="skill-tag inline-block bg-gray-200 text-gray-800 dark:bg-gray-700 dark:text-gray-200 px-3 py-1 rounded-full text-xs font-medium mr-2 mb-2 transition-colors">{children}</span>
+		<span className="skill-tag">{children}</span>
 	);
 };
